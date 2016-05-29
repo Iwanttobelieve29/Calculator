3780258d66e089ff38971825fb1ec8f244dacd78
Iwanttobelieve29 <blavelle19@gmail.com>
2016-05-28 21:32:27 -0400

    Add files via upload

---
+++ calc.rb
@@ -0,0 +1,11 @@
+hourly = 23.70
+hourlyot = hourly * 1.5
+monthlyhr = 120
+monthlyothr = 30
+monthlytotal = hourly * monthlyhr
+monthlytotalot = hourlyot * monthlyothr
+monthly = monthlytotalot + monthlytotal
+yearly = monthly * 12
+
+puts monthly
+puts yearly
